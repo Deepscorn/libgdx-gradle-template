cf4e26b57f15662fe0de3172aaf76fdbcb11730e
Mikhail <vnms11@gmail.com>
2015-05-03 02:06:01 +0400

    init

---
+++ app/src/main/java/com/gamelift/koten/android/AndroidLauncher.java
@@ -0,0 +1,17 @@
+package com.gamelift.koten.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.gamelift.koten.MainGame;
+
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        initialize(new MainGame(), config);
+    }
+}
+
---
+++ desktop/src/main/java/com/gamelift/koten/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package com.gamelift.koten.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gamelift.koten.MainGame;
+
+public class DesktopLauncher {
+    public static void main (String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        new LwjglApplication(new MainGame(), config);
+    }
+}
+
