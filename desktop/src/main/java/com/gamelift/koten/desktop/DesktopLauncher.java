4c3869b9dad404880fb291d1d8b419b5ea9866ad
Mikhail Tokarev <Mikhail.Tokarev@touchin.ru>
2015-05-07 16:47:33 +0300

    clean up

--- desktop/src/main/java/com/gamelift/koten/desktop/DesktopLauncher.java
+++ desktop/src/main/java/com/gamelift/koten/desktop/DesktopLauncher.java
@@ -6,10 +6,11 @@ import com.gamelift.koten.MainGame;
 
 public class DesktopLauncher {
     public static void main (String[] arg) {
+//        //generate items.atlas+png sheets from images from resource/image
 //        TexturePacker.Settings settings = new TexturePacker.Settings();
 //        settings.maxWidth = 512;
 //        settings.maxHeight = 512;
-//        TexturePacker.process(settings, "src", "data", "items");
+//        TexturePacker.process(settings, "../../../../resource/image", "data", "items");
 
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         new LwjglApplication(new MainGame(), config);
